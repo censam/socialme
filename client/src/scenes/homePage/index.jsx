01dea134822ae127db3dcd10f72d90269af547c4
censam <wgsampath@gmail.com>
2023-12-26 07:37:14 +0530

    Navbar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,13 @@
+import { Box } from "@mui/material";
 import React from "react";
+import NavBar from "scenes/navbar";
 
 const HomePage = () => {
- return <div>HomePage</div>;
+ return (
+  <Box>
+   <NavBar />
+  </Box>
+ );
 };
 
 export default HomePage;
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NavBar from "../navbar";
 
 const LoginPage = () => {
- return <div></div>;
+ return (
+  <div>
+   <NavBar />
+   aaa dddd cc
+  </div>
+ );
 };
 
 export default LoginPage;
